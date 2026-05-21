c7dcf97f060cf8b56df44e7a694c8e41988c002f
ananya-1934 <ananyasharma3290@gmail.com>
2026-05-22 01:09:50 +0530

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,25 +1,21 @@
-// js/firebase-init.js
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
-// To get these keys: Go to Firebase Console -> Project Settings -> General -> Web App
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDmePoy_FnlmHi9HfkwEp08eVEBF4SHP34",
+  authDomain: "lost-and-found-bfc0b.firebaseapp.com",
+  projectId: "lost-and-found-bfc0b",
+  storageBucket: "lost-and-found-bfc0b.firebasestorage.app",
+  messagingSenderId: "1089671523647",
+  appId: "1:1089671523647:web:68a257775aed4a441d778a",
+  measurementId: "G-CL8ZQ8GN8Q"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
